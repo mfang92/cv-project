925f9531f31ea9bc08f6cd9ad8b57081272ed512
sarahwang22 <sarahfw@mit.edu>
2024-05-12 16:00:04 -0400

    general dataset that can work with video and images -> possibly replace later

---
+++ 2016/dataset_general.py
@@ -0,0 +1,38 @@
+import os
+import os.path as osp
+import pandas as pd
+from typing import List
+import torch
+
+# more general version of Videos
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, img_dir, read_data, transform=None, target_transform=None, size_lim=None):
+        self.img_dir = img_dir
+        self.read_data = read_data
+        self.transform = transform
+        self.target_transform = target_transform
+        self.size_lim = size_lim
+    
+    @property
+    def fn_list(self) -> List[str]:
+        filenames = [fn for fn in os.listdir(self.img_dir) if not fn.startswith('.') and osp.isfile(osp.join(self.root, fn))]
+        if self.size_lim==None:
+            return filenames
+        else:
+            return filenames[:self.size_lim]
+
+    def __len__(self):
+        return len(self.fn_list)
+
+    def __getitem__(self, idx):
+        file_path = os.path.join(self.img_dir, self.fn_list[idx, 0])
+
+        data = self.read_data(file_path)
+        target = data
+
+        if self.transform:
+            data = self.transform(data)
+        if self.target_transform:
+            target = self.target_transform(target)
+        return data, target
+    
