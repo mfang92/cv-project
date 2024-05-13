86e6e0c325a24f056af532ba5d464223845325ce
sarahwang22 <sarahfw@mit.edu>
2024-05-13 18:16:15 -0400

    evaluate using local data

--- 2016/dataset_general.py
+++ 2016/dataset_general.py
@@ -2,29 +2,51 @@ import os
 import os.path as osp
 from typing import List
 import torch
+import numpy as np
 
 # more general version of Videos
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, read_data, transform=None, target_transform=None, size_lim=None):
-        self.img_dir = img_dir
+    def __init__(self, data_dir, read_data, transform=None, target_transform=None, size_lim=None):
+        """
+            data_dir: path to folder with files; string
+            read_data: function that reads file; (path_to_file: string) -> tensor
+            transform: function that transforms data into input for model; (tensor) -> tensor
+            target_transform: function that transforms data into expected output of model; (tensor) -> tensor
+            size_lim: limit of how many files to include in dataset; number
+        """
+        self.data_dir = data_dir
         self.read_data = read_data
         self.transform = transform
         self.target_transform = target_transform
         self.size_lim = size_lim
-    
-    @property
-    def fn_list(self) -> List[str]:
-        filenames = [fn for fn in os.listdir(self.img_dir) if not fn.startswith('.') and osp.isfile(osp.join(self.img_dir, fn))]
+
+        filenames = np.random.permutation([fn for fn in os.listdir(self.data_dir) if not fn.startswith('.') and osp.isfile(osp.join(self.data_dir, fn))])
         if self.size_lim==None:
-            return filenames
+            self.fn_list = filenames
         else:
-            return filenames[:self.size_lim]
+            self.fn_list = filenames[:self.size_lim]
+
+    def getlist(self):
+        res = []
+        for fn in self.fn_list:
+            data = self.read_data(os.path.join(self.data_dir, fn))
+            target = data
+            
+            if self.transform:
+                data = self.transform(data)
+            if self.target_transform:
+                target = self.target_transform(target)
+
+            res.append(data, target)
+
+        return res
+
 
     def __len__(self):
         return len(self.fn_list)
 
     def __getitem__(self, idx):
-        file_path = os.path.join(self.img_dir, self.fn_list[idx])
+        file_path = os.path.join(self.data_dir, self.fn_list[idx])
 
         data = self.read_data(file_path)
         target = data
